191ba080b011f280e276b4d30655deaea532b135
hessdale <hessdale96@gmail.com>
2023-06-02 20:27:22 -0600

    added exit option through functions

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import mariadb
 import dbcreds
+import sys
 
 conn = mariadb.connect(**dbcreds.conn_params)
 cursor = conn.cursor()
@@ -10,46 +11,72 @@ def get_id():
     password = input('enter password: ')
     cursor.execute('CALL get_user_id(?,?)',[username,password])
     results=cursor.fetchall()
-    cursor.close()
-    conn.close()
-    print(results)
+    print(results[0][0])
+    print('return to menu: 1')
+    print('exit command line blog: 2')
+    selection = input('selection: ')
+    if(int(selection)==1):
+        menu()
+    elif(int(selection)==2):
+        cursor.close()
+        conn.close()
+        sys.exit('Goodbye')
 
 def new_post():
     user_id = input('enter user id: ')
     title = input ('enter title for post: ')
     content = input('enter content for post: ')
     cursor.execute('CALL new_post(?,?,?)',[user_id,title,content])
-    cursor.close()
-    conn.close()
     print('successful post')
+    print('return to menu: 1')
+    print('exit command line blog: 2')
+    selection = input('selection: ')
+    if(int(selection)==1):
+        menu()
+    elif(int(selection)==2):
+        cursor.close()
+        conn.close()
+        sys.exit('Goodbye')
+    
+    
 
 def get_all_posts():
     cursor.execute('CALL get_all_posts()')
     results=cursor.fetchall()
-    cursor.close()
-    conn.close()
     for i in range(len(results)):
         print(results[i][0].decode('utf-8'))
         print(results[i][1].decode('utf-8'))
         print(results[i][2].decode('utf-8'))
+    print('return to menu: 1')
+    print('exit command line blog: 2')
+    selection = input('selection: ')
+    if(int(selection)==1):
+        menu()
+    elif(int(selection)==2):
+        cursor.close()
+        conn.close()
+        sys.exit('Goodbye')
 
 def menu():
-    try:
-        print('to get id of user press 1')
-        print('to create new post press 2')
-        print('to retrieve all posts press 3')
-        print('to exit back to log in press 4')
-        menu_selection = int(input('selection: '))
-        if(menu_selection == 1):
-            get_id()
-        elif(menu_selection == 2):
-            new_post()
-        elif(menu_selection == 3):
-            get_all_posts()
-        elif(menu_selection == 4):
-            login()
-    except:
-        print('something went wrong')
+    print('to get id of user press 1')
+    print('to create new post press 2')
+    print('to retrieve all posts press 3')
+    print('to exit back to log in press 4')
+    print('to exit command line blog press 5')
+    menu_selection = int(input('selection: '))
+    if(menu_selection == 1):
+        get_id()
+    elif(menu_selection == 2):
+        new_post()
+    elif(menu_selection == 3):
+        get_all_posts()
+    elif(menu_selection == 4):
+        login()
+    elif(menu_selection == 5):
+        cursor.close()
+        conn.close()
+        sys.exit('Goodbye')
+
 
 def login(): 
     print('command line blog interface!')
@@ -58,8 +85,6 @@ def login():
     password = input('enter password: ')
     cursor.execute('CALL get_user_id(?,?)',[username,password])
     results=cursor.fetchall()
-    cursor.close()
-    conn.close()
     if (len(results) > 0):
         menu()
     elif(len(results) < 1):
